eab2c89c998ae2f01e04768e1d73ec678c8ab664
Richard Lussier <lussi036@morris.umn.edu>
2021-11-04 11:02:41 -0500

    Implement ExchangeRateReader class
    Implemented the methods inside of ExchangeRateReader, which makes an http request to fixer.io and parses the data returned.
    
    Co-authored-by: helloworld12321 <walbr007@morris.umn.edu>

--- src/main/java/xrate/ExchangeRateReader.java
+++ src/main/java/xrate/ExchangeRateReader.java
@@ -1,6 +1,11 @@
 package xrate;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.json.*;
 
 /**
  * Provide access to basic currency exchange rate services.
@@ -8,6 +13,7 @@ import java.io.IOException;
 public class ExchangeRateReader {
 
     private String accessKey;
+    private String baseURL;
 
     /**
      * Construct an exchange rate reader using the given base URL. All requests will
@@ -20,19 +26,11 @@ public class ExchangeRateReader {
      * @param baseURL the base URL for requests
      */
     public ExchangeRateReader(String baseURL) {
-        /*
-         * DON'T DO MUCH HERE! People often try to do a lot here, but the action is
-         * actually in the two methods below. All you need to do here is store the
-         * provided `baseURL` in a field (which you have to declare) so it will be
-         * accessible in the two key functions. (You'll need it there to construct
-         * the full URL.)
-         */
-
-        // TODO Your code here
+    	
+    	this.baseURL = baseURL;
 
         // Reads the Fixer.io API access key from the appropriate
         // environment variable.
-        // You don't have to change this call.
         readAccessKey();
     }
 
@@ -66,28 +64,7 @@ public class ExchangeRateReader {
      * @throws IOException if there are problems reading from the server
      */
     public float getExchangeRate(String currencyCode, int year, int month, int day) throws IOException {
-        /*
-         * Here you should:
-         * 
-         *   - Construct the appropriate URL
-         *     - This needs to have the date (properly formatted)
-         *       and access key. See the Fixer.io documentation for
-         *       the details.
-         *   - Open a stream from the URL
-         *   - Construct a Tokener from that stream
-         *   - Use that to parse the response into a JSON object
-         *   - Extract the desired currency code from that JSON object
-         *     - Look at the structure of JSON objects returned by Fixer.io.
-         *     - You'll need to extract the "rates" (sub)object from the parsed
-         *       JSON object.
-         *     - You'll need to get the `float` associated with the desired
-         *       currency code from the "rates" object. 
-         */
-
-        // TODO Your code here
-
-        // Remove the next line when you've implemented this method.
-        throw new UnsupportedOperationException();
+    	return getExchangeRate(currencyCode, "EUR", year, month, day);
     }
 
     /**
@@ -104,19 +81,21 @@ public class ExchangeRateReader {
      */
     public float getExchangeRate(String fromCurrency, String toCurrency, int year, int month, int day)
             throws IOException {
-        /*
-         * This is similar to the previous method except that you have to get
-         * the two currency rates and divide one by the other to get their
-         * relative exchange rate.
-         * 
-         * DON'T FORGET HOW TO PROGRAM! Extract helper functions to clarify
-         * what's going on, and try to avoid duplicate logic between this and
-         * the previous method.
-         */
-        
-        // TODO Your code here
-
-        // Remove the next line when you've implemented this method.
-        throw new UnsupportedOperationException();
+    	
+    	String fullURL = String.format(
+    			"%s/%04d-%02d-%02d?access_key=%s&symbols=%s,%s",
+    			baseURL,
+    			year,
+    			month,
+    			day,
+    			URLEncoder.encode(accessKey, "UTF-8"),
+    			URLEncoder.encode(fromCurrency, "UTF-8"),
+    			URLEncoder.encode(toCurrency, "UTF-8"));
+    	URL url = new URL(fullURL);
+    	InputStream inputStream = url.openStream();
+    	
+    	JSONObject json = new JSONObject(new JSONTokener(inputStream));
+    	
+    	return json.getJSONObject("rates").getFloat(fromCurrency) / json.getJSONObject("rates").getFloat(toCurrency);
     }
 }
